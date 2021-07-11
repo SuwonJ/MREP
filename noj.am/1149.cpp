a48fd4292d01a8545870ecadcef9912818cffac8
SuwonJ <su.wnjng@gmail.com>
2021-07-11 02:00:00 +0000

    .

---
+++ noj.am/1149.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int sum;
+char lt;
+int main(void){
+    int N;
+    cin>>N;
+    for(int j=0;j<3;j++){
+        for(int i=0;i<N;i++){
+            int R,G,B,min==1001,mi;
+            cin>>R>>G>>B;
+            if()
+            sum+=
+            lt="R";
+        }
+    }
+   
+}
