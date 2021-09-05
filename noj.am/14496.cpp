0d9abbecc40313a684a9a807b9d24efd33974734
SuwonJ <su.wnjng@gmail.com>
2021-09-05 11:25:49 +0900

    ..

--- noj.am/14496.cpp
+++ noj.am/14496.cpp
@@ -35,16 +35,18 @@ int main(void){
     for(int i=0;i<N+1;i++){
         minD[i]=INTS;   
     }
-    for(int i=0;i<M;i++){
-        cin>>arr[M];
-    } 
-    dijst(A);
-    int nx=1;
-    for(int i=1;i<1000001;i++){
-        if(minD[i]==B){
-            nx=0;
-            cout<<i<<"\n";
-        }
+    for(int i=1;i<M+1;i++){
+         int tmpu,tmpv;
+        cin>>tmpu>>tmpv;
+        vec[tmpu].push_back(make_pair(1,tmpv));
+        vec[tmpv].push_back(make_pair(1,tmpu));
     }
+    dijst(A);
+    if(minD[B]==INTS){
+        cout<<-1;
+        return 0;
+    }
+    int nx=1;
+    cout<<minD[B];
     return 0;
 }
