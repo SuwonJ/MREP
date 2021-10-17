ff1246693982dd08090e90c23b24648e35b325d5
SuwonJ <su.wnjng@gmail.com>
2021-10-17 10:34:43 +0900

    ...

---
+++ TMP/tmp.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <time.h>
+using namespace std;
+
+int main(void) {
+	int i;
+    /*
+    
+    */
+    int a,b;
+    cin>>a>>b;
+    for(int i=0;i<100;i++){
+        cout<<a+b;
+    }
+}
